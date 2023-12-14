07c815f1300bc47d2cf25df10426621d6a1e66f7
ViniMSousa <vini.hmt13@gmail.com>
2023-12-14 02:29:09 -0300

    first commit

---
+++ src/CaixaEletronico.java
@@ -0,0 +1,13 @@
+public class CaixaEletronico {
+    public static void main(String[] args) throws Exception {
+        double saldo = 25.0;
+        double valorSolicitado = 22.0;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo Saldo: " + saldo);
+        } else
+            System.out.println("Saldo insuficiente!");
+
+    }
+}
