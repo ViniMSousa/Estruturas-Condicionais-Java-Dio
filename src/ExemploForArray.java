3b5efe26d2090caa69508ebfee784374a84ac9a1
ViniMSousa <vini.hmt13@gmail.com>
2023-12-19 01:58:10 -0300

    estruturas-repeticao

---
+++ src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (String aluno : alunos) {
+            // System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
